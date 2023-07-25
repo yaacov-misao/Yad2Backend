d51aebb64cb9fb508f0557a2725b22caa3d62861
yaacov-misao <140333532+yaacov-misao@users.noreply.github.com>
2023-07-25 21:09:56 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
 
-app.use('/api/cars', require('/routes/carRoutes'))
-app.use('/api/users', require('/routes/userRoutes'))
+app.use('/api/cars', require('./routes/carRoutes'))
+app.use('/api/users', require('./routes/userRoutes'))
 
 //app.use(errorHandler)
 
