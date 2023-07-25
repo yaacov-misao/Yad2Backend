a5d1896a04bc2aeab7204f46690e1e3a160355b3
yaacov-misao <140333532+yaacov-misao@users.noreply.github.com>
2023-07-25 20:58:28 +0300

    Update server.js

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.use(express.urlencoded({ extended: false}))
 app.use('/api/cars', require('./routes/carRoutes'))
 //app.use('/api/users', require('./routes/userRoutes'))
 
-app.use(errorHandler)
+//app.use(errorHandler)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
