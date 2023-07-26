83b6ce175cd4a02f9881785e7902eeef52d14938
yaacov-misao <140333532+yaacov-misao@users.noreply.github.com>
2023-07-26 12:46:45 +0300

    Update server.js

--- server.js
+++ server.js
@@ -12,15 +12,7 @@ const app = express();
 
 // Allow requests from specific origins
 const allowedOrigins = ['*'];
-app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  }
-}));
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false}))
