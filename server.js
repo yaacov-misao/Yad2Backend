58c926c24fe64f47fce9a278a4895a30b3be56c7
yaacov-misao <140333532+yaacov-misao@users.noreply.github.com>
2023-07-26 11:48:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const colors = require('colors')
 const dotenv = require('dotenv').config()
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
-const port = process.env.PORT || 5001
+const port = process.env.PORT || 10001
 const cors = require('cors');
 
 connectDB()
